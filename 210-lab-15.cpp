cd1432759a08ecf140c06e92897d5a592cd90168
Hussain <Aahil.Hussain@ejgallo.com>
2024-09-29 16:56:05 -0700

    Initial Commit

---
+++ 210-lab-15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream> 
+#include <string>
+using namespace std;
+
+class Movie{
+    private:
+    string screen_writer;
+    int year_released;
+    string title;
+
+    public:
+    //constructor
+    Movie() : screen_writer(""), year_released(0), title("") {}
+
+    //setters
+    void setScreenWriter()
+    void setYearReleased()
+    void setTitle()
+};
