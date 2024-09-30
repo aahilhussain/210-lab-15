6438a4278e219507af323308662e428d90090b93
Hussain <Aahil.Hussain@ejgallo.com>
2024-09-29 17:08:14 -0700

    2nd commit

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -4,6 +4,8 @@
 #include <string>
 using namespace std;
 
+const int W15 = 15;
+
 class Movie{
     private:
     string screen_writer;
@@ -15,7 +17,18 @@ class Movie{
     Movie() : screen_writer(""), year_released(0), title("") {}
 
     //setters
-    void setScreenWriter()
-    void setYearReleased()
-    void setTitle()
+    void setScreenWriter(const string& writer) { screen_writer = writer;}
+    void setYearReleased(int year) { year_released = year;}
+    void setTitle(const string& t) { title = t; }
+
+    //getters
+    string getScreenWriter() const {return screen_writer;}
+    int getYearReleased() const {return year_released;}
+    string getTitle() {return title;}
+
+    void print() {
+        cout << setw(W15) << "Movie: " << title << endl;
+        cout << setw(W15) << "Year released: " << year_released << "\n";
+        cout << "Screenwriter: " << screen_writer << "\n";
+    }
 };
